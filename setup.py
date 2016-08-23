ba189edfc82aa020378382411aa19dcd4953fda2
Joshua Griffiths <joshua.griffiths@opsview.com>
2016-08-23 16:12:30 +0100

    Remove pyc files and bump version

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ from distutils.core import setup
 setup(
     name='opsviewclient',
     packages=['opsviewclient'],
-    version='0.1',
+    version='0.1.1',
     description='API client for interacting with the Opsview API',
     author='Joshua Griffiths',
     author_email='jgriffiths@ceramyq.com',
     url='https://github.com/jgriffiths1993/python-opsviewclient',
-    download_url='https://github.com/jgriffiths1993/python-opsviewclient/tarball/0.1',
+    download_url='https://github.com/jgriffiths1993/python-opsviewclient/tarball/0.1.1',
     keywords=['opsview'],
     classifiers=[]
 )
