d9e8e4b52009726a7949d5bc9f044b0d58b3c73d
Joshua Griffiths <joshua.griffiths@opsview.com>
2016-08-23 16:17:38 +0100

    Move to pbr

--- setup.py
+++ setup.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from distutils.core import setup
+import setuptools
 
-setup(
-    name='opsviewclient',
-    packages=['opsviewclient'],
-    version='0.1.1',
-    description='API client for interacting with the Opsview API',
-    author='Joshua Griffiths',
-    author_email='jgriffiths@ceramyq.com',
-    url='https://github.com/jgriffiths1993/python-opsviewclient',
-    download_url='https://github.com/jgriffiths1993/python-opsviewclient/tarball/0.1.1',
-    keywords=['opsview'],
-    classifiers=[]
-)
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
+setuptools.setup(setup_requires=['pbr>=1.8'], pbr=True)
+
+# from distutils.core import setup
+#
+# setup(
+#     name='opsviewclient',
+#     packages=['opsviewclient'],
+#     version='0.1.1',
+#     description='API client for interacting with the Opsview API',
+#     author='Joshua Griffiths',
+#     author_email='jgriffiths@ceramyq.com',
+#     url='https://github.com/jgriffiths1993/python-opsviewclient',
+#     download_url='https://github.com/jgriffiths1993/python-opsviewclient/tarball/0.1.1',
+#     keywords=['opsview'],
+#     classifiers=[]
+# )
